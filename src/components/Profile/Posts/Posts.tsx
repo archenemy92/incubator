a0687121608659b7d7c842740ab3ea857c50c1cf
Borshchick <19archenemy92@gmail.com>
2021-06-09 12:09:47 +0300

    create new function, and use ref

--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { PostType } from "../../../redux/store"
+import React, {KeyboardEvent} from "react"
+import {PostType} from "../../../redux/store"
 import styles from "../Profile.module.css"
 import {Post} from "./Post/Post"
 
@@ -8,12 +8,22 @@ type PostsPropsType = {
 }
 export const Posts: React.FC<PostsPropsType> = (props) => {
 
+    let textareaValue = React.createRef<HTMLTextAreaElement>()
+    let addPost = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+        if(e.key === "Enter" && e.shiftKey) {
+            let text = textareaValue.current?.value
+            alert(text)
+        }
+    }
 
-    let post = props.postData.map(p=> <Post key={p.id} message={p.message} like={p.like} dislike={p.dislike} ava={p.img}/>)
+    let post = props.postData.map(p => <Post key={p.id} message={p.message} like={p.like} dislike={p.dislike}
+                                             ava={p.img}/>)
 
     return (
         <div className={styles.post}>
-            <textarea className={styles.textarea}/>
+            <textarea className={styles.textarea}
+                      ref={textareaValue}
+                      onKeyPress={addPost}/>
             <div>
                 {post}
             </div>
