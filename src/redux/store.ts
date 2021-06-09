d069f40c3036a3d428aecfe99beb362f45502154
Borshchick <19archenemy92@gmail.com>
2021-06-09 15:40:14 +0300

    add new functions who create new objects, add new type, import function rerenderTree to realized flux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import {v1} from "uuid"
+import {rerenderTree} from "../rerender"
 
 let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
-
 
 
 export type PostType = {
@@ -28,9 +28,15 @@ export type sidebarItemsType = {
 export type PostDataType = {
     postData: PostType[]
 }
+export type MessagesType = {
+    id: string
+    body: string
+    img: string
+}
 
 export type DialogDataType = {
     dialogData: DialogItemsType[]
+    messages: MessagesType[]
 }
 
 export type SidebarDataType = {
@@ -42,6 +48,13 @@ export type StateType = {
     dialogsPage: DialogDataType
     sidebarPage: SidebarDataType
 }
+/*
+export type StoreType = {
+    state: StateType
+    addPost: (message: string) => void
+    rerender: () => void
+}*/
+
 
 export let state: StateType = {
     profilePage: {
@@ -52,18 +65,22 @@ export let state: StateType = {
             {message: "Hello man", id: v1(), like: 0, dislike: 0, img: ava}
         ]
     },
-   dialogsPage: {
-       dialogData: [
-           {name: "LENKA", id: v1(), img: ava},
-           {name: "YURA", id: v1(), img: ava},
-           {name: "NIKITA", id: v1(), img: ava},
-           {name: "MARGO", id: v1(), img: ava},
-           {name: "ANASTASIYA", id: v1(), img: ava},
-           {name: "OXANA", id: v1(), img: ava},
-           {name: "VOLODYMYR", id: v1(), img: ava},
-           {name: "KARINA", id: v1(), img: ava}
-       ]
-   },
+    dialogsPage: {
+        dialogData: [
+            {name: "LENKA", id: v1(), img: ava},
+            {name: "YURA", id: v1(), img: ava},
+            {name: "NIKITA", id: v1(), img: ava},
+            {name: "MARGO", id: v1(), img: ava},
+            {name: "ANASTASIYA", id: v1(), img: ava},
+            {name: "OXANA", id: v1(), img: ava},
+            {name: "VOLODYMYR", id: v1(), img: ava},
+            {name: "KARINA", id: v1(), img: ava}
+        ],
+        messages: [
+
+        ]
+
+    },
     sidebarPage: {
         sidebarData: [
             {id: v1(), name: "LENKA", img: ava, value: "hey"},
@@ -71,4 +88,19 @@ export let state: StateType = {
             {id: v1(), name: "NIKITA", img: ava, value: "hey"}
         ]
     }
+}
+export const addPost = (message:string) => {
+    let newPost = {
+        message, id: v1(), like: 0, dislike: 0, img: ava
+    }
+    state.profilePage.postData.push(newPost)
+    rerenderTree(state)
+}
+
+export const addMessage=(body: string) => {
+    let newMessage = {
+        body, id: v1(), img: ava
+    }
+    state.dialogsPage.messages.push(newMessage)
+    rerenderTree(state)
 }
