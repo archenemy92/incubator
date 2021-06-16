0f889348e2259696a398d211acd9aaafb0321881
Borshchick <19archenemy92@gmail.com>
2021-06-16 19:47:48 +0300

    implement store instead state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import {v1} from "uuid"
-import {rerenderTree} from "../rerender"
 
 let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
 
@@ -48,59 +47,75 @@ export type StateType = {
     dialogsPage: DialogDataType
     sidebarPage: SidebarDataType
 }
-/*
 export type StoreType = {
-    state: StateType
+    _state: StateType
     addPost: (message: string) => void
-    rerender: () => void
-}*/
+    addMessage: (body: string) => void
+    _callSubscriber: (state: StateType) => void
+    subscribe: (observer: (state: StateType) => void) => void
+    getState: () => StateType
 
-
-export let state: StateType = {
-    profilePage: {
-        postData: [
-            {message: "Hello", id: v1(), like: 0, dislike: 0, img: ava},
-            {message: "Hello my friend", id: v1(), like: 0, dislike: 0, img: ava},
-            {message: "Hello dude", id: v1(), like: 0, dislike: 0, img: ava},
-            {message: "Hello man", id: v1(), like: 0, dislike: 0, img: ava}
-        ]
-    },
-    dialogsPage: {
-        dialogData: [
-            {name: "LENKA", id: v1(), img: ava},
-            {name: "YURA", id: v1(), img: ava},
-            {name: "NIKITA", id: v1(), img: ava},
-            {name: "MARGO", id: v1(), img: ava},
-            {name: "ANASTASIYA", id: v1(), img: ava},
-            {name: "OXANA", id: v1(), img: ava},
-            {name: "VOLODYMYR", id: v1(), img: ava},
-            {name: "KARINA", id: v1(), img: ava}
-        ],
-        messages: [
-
-        ]
-
-    },
-    sidebarPage: {
-        sidebarData: [
-            {id: v1(), name: "LENKA", img: ava, value: "hey"},
-            {id: v1(), name: "MARGO", img: ava, value: "hey"},
-            {id: v1(), name: "NIKITA", img: ava, value: "hey"}
-        ]
-    }
-}
-export const addPost = (message:string) => {
-    let newPost = {
-        message, id: v1(), like: 0, dislike: 0, img: ava
-    }
-    state.profilePage.postData.push(newPost)
-    rerenderTree(state)
 }
 
-export const addMessage=(body: string) => {
-    let newMessage = {
-        body, id: v1(), img: ava
+export const store: StoreType = {
+    _state: {
+        profilePage: {
+            postData: [
+                {message: "Hello", id: v1(), like: 0, dislike: 0, img: ava},
+                {message: "Hello my friend", id: v1(), like: 0, dislike: 0, img: ava},
+                {message: "Hello dude", id: v1(), like: 0, dislike: 0, img: ava},
+                {message: "Hello man", id: v1(), like: 0, dislike: 0, img: ava}
+            ]
+        },
+        dialogsPage: {
+            dialogData: [
+                {name: "LENKA", id: v1(), img: ava},
+                {name: "YURA", id: v1(), img: ava},
+                {name: "NIKITA", id: v1(), img: ava},
+                {name: "MARGO", id: v1(), img: ava},
+                {name: "ANASTASIYA", id: v1(), img: ava},
+                {name: "OXANA", id: v1(), img: ava},
+                {name: "VOLODYMYR", id: v1(), img: ava},
+                {name: "KARINA", id: v1(), img: ava}
+            ],
+            messages: []
+
+        },
+        sidebarPage: {
+            sidebarData: [
+                {id: v1(), name: "LENKA", img: ava, value: "hey"},
+                {id: v1(), name: "MARGO", img: ava, value: "hey"},
+                {id: v1(), name: "NIKITA", img: ava, value: "hey"}
+            ]
+        }
+    },
+
+    getState(){
+        return this._state
+    },
+
+    addPost(message: string) {
+        let newPost = {
+            message, id: v1(), like: 0, dislike: 0, img: ava
+        }
+        this._state.profilePage.postData.push(newPost)
+        this._callSubscriber(this._state)
+    },
+
+    addMessage(body: string) {
+        let newMessage = {
+            body, id: v1(), img: ava
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._callSubscriber(this._state)
+    },
+
+    subscribe(observer: (state: StateType) => void) {
+        this._callSubscriber = observer
+    },
+
+
+    _callSubscriber(state: StateType) {
+        console.log("state is changed")
     }
-    state.dialogsPage.messages.push(newMessage)
-    rerenderTree(state)
 }
