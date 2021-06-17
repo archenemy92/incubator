ae93877f21205f6f3d2727fd4287efe28fbb27e4
Borshchick <19archenemy92@gmail.com>
2021-06-17 19:34:17 +0300

    use dispatch

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,18 +2,18 @@ import React from "react"
 import {AvaDesc} from "./AvaDesc"
 import {Posts} from "./Posts/Posts"
 import styles from "./Profile.module.css"
-import {PostType} from "../../redux/store"
+import {ActionsType, PostType} from "../../redux/store"
 
 
 type ProfilePropsType = {
     postData: PostType[]
-    addPost: (message: string)=>void
+    dispatch: (action: ActionsType) => void
 }
 export const Profile: React.FC<ProfilePropsType> = (props) => {
     return (
         <div className={styles.content}>
             <AvaDesc/>
-            <Posts postData={props.postData} addPost={props.addPost}/>
+            <Posts postData={props.postData} dispatch={props.dispatch}/>
         </div>
     )
 }
