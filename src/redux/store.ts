c357baba5f5e22fda2e2487d852610cf469b495d
Borshchick <19archenemy92@gmail.com>
2021-07-12 20:57:52 +0300

    add redux store, and types to it, refactor

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
-import {v1} from "uuid"
+
 import {AddPostType, profileReducer} from "./profileReducer"
 import {AddMessageType, dialogReducer} from "./dialogsReducer"
 import {sidebarReducer} from "./sidebarReducer"
+import {combineReducers, createStore } from "redux"
+
 
 export let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
 
@@ -50,70 +52,18 @@ export type StateType = {
     dialogsPage: DialogDataType
     sidebarPage: SidebarDataType
 }
-export type StoreType = {
-    _state: StateType
-    _callSubscriber: (state: StateType) => void
-    subscribe: (observer: (state: StateType) => void) => void
-    getState: () => StateType
-    dispatch: (action: ActionsType) => void
-}
+
 
 
 export type ActionsType = AddPostType| AddMessageType
 
+type RootReducerType = typeof rootReducer
+export type StoreType = ReturnType<RootReducerType>
 
+let rootReducer = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogReducer,
+    sidebarPage: sidebarReducer
+})
 
-export const store: StoreType = {
-    _state: {
-        profilePage: {
-            postData: [
-                {message: "Hello", id: v1(), like: 0, dislike: 0, img: ava},
-                {message: "Hello my friend", id: v1(), like: 0, dislike: 0, img: ava},
-                {message: "Hello dude", id: v1(), like: 0, dislike: 0, img: ava},
-                {message: "Hello man", id: v1(), like: 0, dislike: 0, img: ava}
-            ]
-        },
-        dialogsPage: {
-            dialogData: [
-                {name: "LENKA", id: v1(), img: ava},
-                {name: "YURA", id: v1(), img: ava},
-                {name: "NIKITA", id: v1(), img: ava},
-                {name: "MARGO", id: v1(), img: ava},
-                {name: "ANASTASIYA", id: v1(), img: ava},
-                {name: "OXANA", id: v1(), img: ava},
-                {name: "VOLODYMYR", id: v1(), img: ava},
-                {name: "KARINA", id: v1(), img: ava}
-            ],
-            messages: []
-
-        },
-        sidebarPage: {
-            sidebarData: [
-                {id: v1(), name: "LENKA", img: ava, value: "hey"},
-                {id: v1(), name: "MARGO", img: ava, value: "hey"},
-                {id: v1(), name: "NIKITA", img: ava, value: "hey"}
-            ]
-        }
-    },
-
-    getState() {
-        return this._state
-    },
-    dispatch(action: ActionsType) {
-        this._state.profilePage = profileReducer( this._state.profilePage, action)
-        this._state.dialogsPage = dialogReducer(this._state.dialogsPage, action)
-        this._state.sidebarPage = sidebarReducer(this._state.sidebarPage, action)
-
-        this._callSubscriber(this._state)
-
-    },
-
-    subscribe(observer: (state: StateType) => void) {
-        this._callSubscriber = observer
-    },
-
-
-    _callSubscriber(state: StateType) {
-        console.log("state is changed")
-    }
-}
+export let store = createStore(rootReducer)
