02a0a185851f7ecc0fa8ac87cdd27a1a0894af4f
Borshchick <19archenemy92@gmail.com>
2021-07-12 20:58:39 +0300

    add callback to subscribe

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import {BrowserRouter} from "react-router-dom"
-import {StateType, store} from "./redux/store"
+import {store, StoreType} from "./redux/store"
 
- const rerenderTree = (state:StateType): void => {
+ const rerenderTree = (state:StoreType): void => {
     ReactDOM.render(
         <BrowserRouter>
             <App state={state} dispatch={store.dispatch.bind(store)}/>
@@ -16,7 +16,10 @@ import {StateType, store} from "./redux/store"
 }
 rerenderTree(store.getState())
 
-store.subscribe(rerenderTree)
+store.subscribe(() =>{
+    let state: StoreType = store.getState()
+    rerenderTree(state)}
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
