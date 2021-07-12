294b39abf8301c0ec616de94aea18ecdd0caa38b
Borshchick <19archenemy92@gmail.com>
2021-07-12 20:55:45 +0300

    add init state

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,12 +1,13 @@
-import {ActionsType, ava, PostDataType} from "./store"
+import {ActionsType, PostDataType} from "./store"
 import {v1} from "uuid"
 
+export const PROFILE_ADD_POST = "PROFILE_ADD_POST"
+let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
 
 export type AddPostType = {
     type: typeof PROFILE_ADD_POST
     message: string
 }
-export const PROFILE_ADD_POST = "PROFILE_ADD_POST"
 
 export const addPostAC = (message: string): AddPostType => {
     return {
@@ -15,7 +16,16 @@ export const addPostAC = (message: string): AddPostType => {
     }
 }
 
-export const profileReducer = (state:PostDataType, action: ActionsType): PostDataType =>{
+const initState: PostDataType = {
+    postData: [
+        {message: "Hello", id: v1(), like: 0, dislike: 0, img: ava},
+        {message: "Hello my friend", id: v1(), like: 0, dislike: 0, img: ava},
+        {message: "Hello dude", id: v1(), like: 0, dislike: 0, img: ava},
+        {message: "Hello man", id: v1(), like: 0, dislike: 0, img: ava}
+    ]
+}
+
+export const profileReducer = (state = initState, action: ActionsType): PostDataType =>{
     switch (action.type) {
         case PROFILE_ADD_POST:
             let newPost = {
--- src/redux/sidebarReducer.ts
+++ src/redux/sidebarReducer.ts
@@ -1,5 +1,15 @@
 import {ActionsType, SidebarDataType} from "./store"
+import {v1} from "uuid"
 
-export const sidebarReducer = (state: SidebarDataType, action: ActionsType): SidebarDataType => {
+let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
+
+const initState: SidebarDataType= {
+    sidebarData: [
+        {id: v1(), name: "LENKA", img: ava, value: "hey"},
+        {id: v1(), name: "MARGO", img: ava, value: "hey"},
+        {id: v1(), name: "NIKITA", img: ava, value: "hey"}
+    ]
+}
+export const sidebarReducer = (state = initState, action: ActionsType): SidebarDataType => {
     return state
 }
