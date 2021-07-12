a260efd312cea9be371bfab467df656c50385154
Borshchick <19archenemy92@gmail.com>
2021-07-12 20:55:31 +0300

    add init state

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -1,5 +1,5 @@
 import {v1} from "uuid"
-import {ActionsType, ava, DialogDataType} from "./store"
+import {ActionsType, DialogDataType} from "./store"
 
 export type AddMessageType = {
     type: typeof DIALOGS_ADD_MESSAGE
@@ -13,8 +13,23 @@ export const addMessageAC = (body: string): AddMessageType => {
         body
     }
 }
+let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
 
-export const dialogReducer = (state: DialogDataType, action: ActionsType): DialogDataType => {
+const initState: DialogDataType = {
+    dialogData: [
+        {name: "LENKA", id: v1(), img: ava},
+        {name: "YURA", id: v1(), img: ava},
+        {name: "NIKITA", id: v1(), img: ava},
+        {name: "MARGO", id: v1(), img: ava},
+        {name: "ANASTASIYA", id: v1(), img: ava},
+        {name: "OXANA", id: v1(), img: ava},
+        {name: "VOLODYMYR", id: v1(), img: ava},
+        {name: "KARINA", id: v1(), img: ava}
+    ],
+    messages: []
+}
+
+export const dialogReducer = (state = initState, action: ActionsType): DialogDataType => {
     switch (action.type) {
         case DIALOGS_ADD_MESSAGE:
             let newMessage = {
