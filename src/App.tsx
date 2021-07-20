1570e2221869b9c15404011aaf6d7392c14bc71f
Borshchick <19archenemy92@gmail.com>
2021-07-20 20:16:24 +0300

    use UsersContainer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import "./App.css"
 import {Header} from "./components/Header/Header"
 import {Profile} from "./components/Profile/Profile"
-import {Users} from "./components/Users/Users"
 import {Friends} from "./components/Friends/Friends"
 import {Music} from "./components/Music/Music"
 import {Settings} from "./components/Settings/Settings"
 import {Route} from "react-router-dom"
 import {DialogsContainer} from "./components/Dialogs/DialogsContainer"
 import {NavbarContainer} from "./components/Navbar/NavbarContainer"
+import {UsersContainer} from "./components/Users/UsersContainer"
 
 
 export type AppPropsType = {}
@@ -21,7 +21,7 @@ const App: React.FC<AppPropsType> = () => {
             <div className={"appWrapperContent"}>
                 <Route path={"/profile"} render={() => <Profile/>}/>
                 <Route path={"/dialogs"} render={() => <DialogsContainer/>}/>
-                <Route path={"/users"} render={() => <Users/>}/>
+                <Route path={"/users"} render={() => <UsersContainer/>}/>
                 <Route path={"/friends"} render={() => <Friends/>}/>
                 <Route path={"/music"} render={() => <Music/>}/>
                 <Route path={"/settings"} render={() => <Settings/>}/>
