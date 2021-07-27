0005fd1635b40fc7b7cad128aea3447f52821bb1
Borshchick <19archenemy92@gmail.com>
2021-07-27 17:51:58 +0300

    create axios request

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import {UsersType} from "../../redux/usersReducer"
-import {v1} from "uuid"
 import classes from "./Users.module.css"
+import axios from "axios"
 
 let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
 
@@ -14,48 +14,10 @@ type UsersPropsType = {
 
 export const Users: React.FC<UsersPropsType> = (props) => {
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                name: "Shubert",
-                id: v1(),
-                photos: {
-                    small: null,
-                    large: null
-                },
-                status: null,
-                followed: true
-            }, {
-                name: "Saskia",
-                id: v1(),
-                photos: {
-                    small: null,
-                    large: null
-                },
-                status: null,
-                followed: false
-            }, {
-                name: "Morgan",
-                id: v1(),
-                photos: {
-                    small: null,
-                    large: null
-                },
-                status: null,
-                followed: false
-            }, {
-                name: "Luisa",
-                id: v1(),
-                photos: {
-                    small: null,
-                    large: null
-                },
-                status: null,
-                followed: false
-            },
-        ])
-    }
-
+if (props.users.length === 0) {
+    axios.get<{items: UsersType[]}>("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response)=>props.setUsers(response.data.items))
+}
 
     return (
         <>
@@ -71,7 +33,7 @@ export const Users: React.FC<UsersPropsType> = (props) => {
                 return <div key={user.id} className={classes.user_container}>
                     <div className={classes.user_content}>
                         <div className={classes.user_ava}>
-                            <img src={user.photos.small? user.photos.small: ava} alt={"avatar must be here"}/>
+                            <img src={user.photos.small ? user.photos.small : ava} alt={"avatar must be here"}/>
                         </div>
                         <div className={classes.user_name}>
                             {user.name}
@@ -80,11 +42,11 @@ export const Users: React.FC<UsersPropsType> = (props) => {
                             descr
                         </div>
                     </div>
-                        {
-                            user.followed
-                                ? <button onClick={unfollow}>follow</button>
-                                : <button onClick={follow}>unfollow</button>
-                        }
+                    {
+                        user.followed
+                            ? <button onClick={unfollow}>follow</button>
+                            : <button onClick={follow}>unfollow</button>
+                    }
                 </div>
             })
             }
