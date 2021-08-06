5b648131d88fd819bb1b5e6fb62caf95d9a4cea1
Borshchick <19archenemy92@gmail.com>
2021-08-06 16:41:04 +0300

    implement new logic

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,22 +7,55 @@ let ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebk
 
 type UsersPropsType = {
     users: UsersType[]
+    totalCount: number
+    currentPage: number
+    pageSize: number
     follow: (userID: string) => void
     unfollow: (userID: string) => void
     setUsers: (users: UsersType[]) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalCount: (count: number) => void
+
 }
 
 export class Users extends React.Component<UsersPropsType> {
 
     componentDidMount() {
-        axios.get<{ items: UsersType[] }>("https://social-network.samuraijs.com/api/1.0/users")
-            .then((response) => this.props.setUsers(response.data.items))
+        axios.get<{ items: UsersType[], totalCount: number }>(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalCount(response.data.totalCount)
+            })
     }
 
+    onPageChangeHandler = (currentPage: number) => {
+        this.props.setCurrentPage(currentPage)
+        axios.get<{ items: UsersType[] }>(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+
     render() {
+        let pagesCount = 15 //Math.ceil(this.props.totalCount / this.props.pageSize)
+        let pages = []
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return (
             <>
-
+                <div>
+                    {pages.map((p, i) => <span
+                        key={i}
+                        className={this.props.currentPage === p
+                            ? classes.paginationSelected
+                            : classes.pagination}
+                        onClick={() => this.onPageChangeHandler(p)}>
+                        {p}
+                    </span>)}
+                </div>
                 {this.props.users.map(user => {
 
                     const unfollow = () => {
