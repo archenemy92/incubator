b97849eaf4352f20274daa3acbfba31560852685
Borshchick <19archenemy92@gmail.com>
2021-08-06 17:19:10 +0300

    refactor=>move class component to connect component

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -2,7 +2,60 @@ import {connect} from "react-redux"
 import {Users} from "./Users"
 import {ActionsType, StateType} from "../../redux/store"
 import {follow, setCurrentPage, setTotalCount, setUsers, unfollow, UsersType} from "../../redux/usersReducer"
-import {Dispatch} from "react"
+import React, {Dispatch} from "react"
+import axios from "axios"
+
+type UsersCPropsType = {
+    users: UsersType[]
+    totalCount: number
+    currentPage: number
+    pageSize: number
+    follow: (userID: string) => void
+    unfollow: (userID: string) => void
+    setUsers: (users: UsersType[]) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalCount: (count: number) => void
+
+}
+
+export class UsersC extends React.Component<UsersCPropsType> {
+
+    componentDidMount() {
+        axios.get<{ items: UsersType[], totalCount: number }>(
+            `https://social-network.samuraijs.com/api/1.0/users?
+            page=${this.props.currentPage}&count=${this.props.pageSize}`
+        )
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalCount(response.data.totalCount)
+            })
+    }
+
+    onPageChangeHandler = (currentPage: number) => {
+        this.props.setCurrentPage(currentPage)
+        axios.get<{ items: UsersType[] }>(
+            `https://social-network.samuraijs.com/api/1.0/users?
+            page=${currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+
+        return (
+            <Users users={this.props.users}
+                   currentPage={this.props.currentPage}
+                   totalCount={this.props.totalCount}
+                   pageSize={this.props.pageSize}
+                   onPageChangeHandler={this.onPageChangeHandler}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}/>
+        )
+    }
+
+}
+
 
 type MDTPType = {
     follow: (userID: string) => void
@@ -48,5 +101,4 @@ const mapDispatchToProps = (dispatch: Dispatch<ActionsType>): MDTPType => {
     }
 }
 
-
-export const UsersContainer = connect<MSTPType, MDTPType, {}, StateType>(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect<MSTPType, MDTPType, {}, StateType>(mapStateToProps, mapDispatchToProps)(UsersC)
