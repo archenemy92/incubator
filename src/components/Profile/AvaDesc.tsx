31cc4ab9c0182099c7a12117ed8d66d1091e0dc2
Borshchick <19archenemy92@gmail.com>
2021-08-06 22:29:34 +0300

    add info

--- src/components/Profile/AvaDesc.tsx
+++ src/components/Profile/AvaDesc.tsx
@@ -1,16 +1,32 @@
 import React from "react"
 import styles from "./Profile.module.css"
+import {ProfileType} from "../../redux/store"
+import {Preloader} from "../Common/Preloader/Preloader"
 
-export const AvaDesc = () => {
+
+type AvaDescType = {
+    profile: ProfileType | null
+}
+
+const ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
+
+export const AvaDesc:React.FC<AvaDescType> = ({profile}) => {
+    console.log(profile)
+    if (!profile) {
+        return <Preloader/>
+    }
+
     return (
         <div className={styles.avaDesc}>
             <div>
                 <img className={styles.ava}
-                     src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"}
-                     alt={"name"}/>
+                     src={profile.photos.large? profile.photos.large : ava}
+                     alt={"profile photo"}/>
             </div>
             <div className={styles.desc}>
-                desc
+               <span>{profile.aboutMe}</span>
+               <span>{profile.fullName}</span>
+               <span>{profile.lookingForAJobDescription}</span>
             </div>
         </div>
     )
