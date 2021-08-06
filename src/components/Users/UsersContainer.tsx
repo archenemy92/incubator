7b27c0f0bc7b48d5080fb17a08e93c49aac4600b
Borshchick <19archenemy92@gmail.com>
2021-08-06 16:41:36 +0300

    add new props

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,28 +1,36 @@
 import {connect} from "react-redux"
 import {Users} from "./Users"
 import {ActionsType, StateType} from "../../redux/store"
-import {follow, setUsers, unfollow, UsersType} from "../../redux/usersReducer"
+import {follow, setCurrentPage, setTotalCount, setUsers, unfollow, UsersType} from "../../redux/usersReducer"
 import {Dispatch} from "react"
 
 type MDTPType = {
-    follow: (userID: string)=> void
+    follow: (userID: string) => void
     unfollow: (userID: string) => void
     setUsers: (users: UsersType[]) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalCount: (count: number) => void
 }
 
 type MSTPType = {
     users: UsersType[]
+    totalCount: number
+    currentPage: number
+    pageSize: number
 }
 
 const mapStateToProps = (state: StateType): MSTPType => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        totalCount: state.usersPage.totalCount,
+        currentPage: state.usersPage.currentPage,
+        pageSize: state.usersPage.pageSize
     }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch<ActionsType>): MDTPType => {
     return {
-        follow:(userID: string) => {
+        follow: (userID: string) => {
             dispatch(follow(userID))
         },
         unfollow: (userID: string) => {
@@ -30,6 +38,12 @@ const mapDispatchToProps = (dispatch: Dispatch<ActionsType>): MDTPType => {
         },
         setUsers: (users: UsersType[]) => {
             dispatch(setUsers(users))
+        },
+        setCurrentPage: (currentPage: number) => {
+            dispatch(setCurrentPage(currentPage))
+        },
+        setTotalCount: (count: number) => {
+            dispatch(setTotalCount(count))
         }
     }
 }
