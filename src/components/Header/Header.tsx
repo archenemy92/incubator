8295eb3704202d355c5c75364bc86a8d47368f17
Borshchick <19archenemy92@gmail.com>
2021-08-09 23:23:40 +0300

    create props type

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
 import React from "react"
 import style from "./Header.module.css"
 
-export const Header = () => {
+type HeaderPropsType = {
+    login: string | null
+    isAuth: boolean
+}
+
+export const Header: React.FC<HeaderPropsType> = ({login, isAuth}) => {
+
     return (
         <header className={style.header}>
             <img className={style.img}
                  alt={"logo"}
                  src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTyFmMV4e7Cgm3rAIKLnsrDkahJPFWL2wwIg&usqp=CAU"}/>
             <input className={style.input}/>
+            {isAuth && <div className={style.login}>{login}</div>}
         </header>
     )
 }
