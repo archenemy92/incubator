65d4947f0265321f00d4244b72329b38236a5b08
Borshchick <19archenemy92@gmail.com>
2021-08-24 15:48:45 +0300

    create new Component

--- src/components/Profile/AvaDesc.tsx
+++ src/components/Profile/AvaDesc.tsx
@@ -1,16 +1,14 @@
-import React from "react"
+import React, {ChangeEvent} from "react"
 import styles from "./Profile.module.css"
 import {ProfileType} from "../../redux/store"
 import {Preloader} from "../Common/Preloader/Preloader"
-
+import ava from "../../accets/avatar.png"
 
 type AvaDescType = {
     profile: ProfileType | null
 }
 
-const ava = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrrxsxZSpsfebkw8VLXe6R5j7mryT6PK7Pg&usqp=CAU"
-
-export const AvaDesc:React.FC<AvaDescType> = ({profile}) => {
+export const AvaDesc: React.FC<AvaDescType> = ({profile}) => {
     if (!profile) {
         return <Preloader/>
     }
@@ -18,14 +16,59 @@ export const AvaDesc:React.FC<AvaDescType> = ({profile}) => {
         <div className={styles.avaDesc}>
             <div>
                 <img className={styles.ava}
-                     src={profile.photos.large? profile.photos.large : ava}
+                     src={profile.photos.large ? profile.photos.large : ava}
                      alt={"profileImage"}/>
+                <Status/>
             </div>
             <div className={styles.desc}>
-               <span>{profile.aboutMe}</span>
-               <span>{profile.fullName}</span>
-               <span>{profile.lookingForAJobDescription}</span>
+                <span>{profile.aboutMe}</span>
+                <span>{profile.fullName}</span>
+                <span>{profile.lookingForAJobDescription}</span>
             </div>
         </div>
     )
+}
+
+type StateType = {
+    editeMode: boolean
+    value: string
+}
+
+class Status extends React.Component {
+    state: StateType = {
+        editeMode: false,
+        value: "value"
+    }
+
+    isEditeMode = () => {
+        this.setState({
+            editeMode: !this.state.editeMode
+        })
+    }
+
+    onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            value: e.currentTarget.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.editeMode
+                    ? <div>
+                        <span onDoubleClick={this.isEditeMode}>{this.state.value}</span>
+                    </div>
+                    : <div>
+                        <input
+                            autoFocus
+                            onBlur={this.isEditeMode}
+                            value={this.state.value}
+                            onChange={this.onChangeHandler}
+                        />
+                    </div>
+                }
+            </div>
+        )
+    }
 }
